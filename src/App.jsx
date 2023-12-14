a71ca2506145aa2b9fa3116fb3c17129183403d4
KhitemMed <lk_medfouni@esi.dz>
2023-12-14 08:30:58 +0100

    Update App.jsx
    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">hello IGL</h1>
+      <h1 className="text-red-500">hello khitem</h1>
     </div>
   )
 }
