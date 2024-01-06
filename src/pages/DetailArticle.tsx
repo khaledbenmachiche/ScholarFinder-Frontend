7389dcb94be0ec2cc47bd2ef7b12780c4f21b233
Benmachiche Khaled <lk_benmachiche@esi.dz>
2024-01-06 01:14:06 +0100

    integration article detail

--- src/pages/DetailArticle.tsx
+++ src/pages/DetailArticle.tsx
@@ -2,10 +2,10 @@ import {FaRegFilePdf} from "react-icons/fa6";
 import {MdFavoriteBorder} from "react-icons/md";
 import {MdOutlineNavigateNext} from "react-icons/md";
 import quotes from "../assets/quotes.svg" ;
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {GrMenu} from "react-icons/gr";
 import useAxios from "../hooks/useAxios";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import NavBarUtilisateur from "../components/NavBarUtilisateur";
 
 interface Institution {
@@ -18,10 +18,12 @@ interface Auteur {
     nom: string;
     institutions: Institution[];
 }
+
 interface MotCle {
     id: number;
     text: string;
 }
+
 interface ReferenceBibliographique {
     id: number;
     nom: string;
@@ -41,7 +43,7 @@ interface Article {
 const DetailArticle = () => {
     const {id} = useParams<{ id: string }>();
     const axios = useAxios();
-    const [article, setArticle] = useState<Article|null>(null);
+    const [article, setArticle] = useState<Article | null>(null);
     useEffect(() => {
         axios.get<Article>(`/articles/${id}`).then((response) => {
             setArticle(response.data);
@@ -49,85 +51,92 @@ const DetailArticle = () => {
     }, [id]);
 
     const openPdf = () => {
-        if(article?.url){
+        if (article?.url) {
             window.location.href = article.url;
-        }else{
+        } else {
             alert("Pas de pdf pour cet article");
         }
     }
     return (
 
         <div className="w-screen h-screen ">
-            <NavBarUtilisateur/>
-            <h1 className="font-poppins font-bold text-blue-600 ml-28 lg:mt-12 lg:ml-[420px] text-sm  lg:text-2xl  "> L'infini
-                du savoir vous attend à portée de clic.</h1>
-            <div className="flex w-screen p-3 mt-2 bg-gray-100 border pl-14 ">
+            <div className='h-72 flex flex-col bg-[#EEF5FC]'>
+                <NavBarUtilisateur/>
+                <p className='my-auto text-2xl md:text-4xl font-semibold text-[#0053AD] text-center'>L'INFINI DU
+                    SAVOIR VOUS ATTEND A PORTEE DE CLIC.
+                </p>
+            </div>
+            <div className="flex w-screen p-3 bg-gray-100 border pl-14 ">
                 <h4>Acceuil</h4> <MdOutlineNavigateNext size={20} className="relative top-1"/> <h4>Recherche</h4>
                 <MdOutlineNavigateNext size={20} className="relative top-1"/> <h4>Article detail</h4>
             </div>
             <div className="flex justify-center gap-5 pr-6 mt-4 lg:justify-end">
-                <button onClick={openPdf} className="flex items-center p-2 bg-gray-300 whitespace-nowrap"><FaRegFilePdf size={20}/> Ouvrir sous forme pdf</button>
-                <button className="flex items-center p-2 bg-gray-300 whitespace-nowrap"><MdFavoriteBorder size={20}/> AJOUTER AUX FAVORIS</button>
+                <button onClick={openPdf} className="flex items-center p-2 bg-gray-300 whitespace-nowrap"><FaRegFilePdf
+                    size={20}/> Ouvrir sous forme pdf
+                </button>
+                <button className="flex items-center p-2 bg-gray-300 whitespace-nowrap"><MdFavoriteBorder
+                    size={20}/> AJOUTER AUX FAVORIS
+                </button>
             </div>
-                        <div className="flex items-center py-4  lg:mt-5 lg:ml-20 space-x-7 w-1/2 lg:w[1180px] mt-5 ml-12  ">
-                            <img className="w-14 " alt="quote" src={quotes}/>
-                            <div className="h">
-                                <h1 className="text-2xl font-bold font-poppins">{article?.titre}</h1>
-                                <div className="w-full h-px border-b border-black border-solid"></div>
-                            </div>
-                        </div>
+            <div className="flex items-center py-4  lg:mt-5 lg:ml-20 space-x-7 w-1/2 lg:w[1180px] mt-5 ml-12  ">
+                <img className="w-14 " alt="quote" src={quotes}/>
+                <div className="h">
+                    <h1 className="text-2xl font-bold font-poppins">{article?.titre}</h1>
+                    <div className="w-full h-px border-b border-black border-solid"></div>
+                </div>
+            </div>
 
-                        <div className="mt-5 ml-12 lg:ml-20">
-                            <h6 className="font-poppins "> Date de publication : {article?.date_de_publication}</h6>
-                            <br/>
-                            <div>
-                                {
-                                    article?.auteurs.map(auteur => {
+            <div className="mt-5 ml-12 lg:ml-20">
+                <h6 className="font-poppins "> Date de publication : {article?.date_de_publication}</h6>
+                <br/>
+                <div>
+                    {
+                        article?.auteurs.map(auteur => {
+                            return (
+                                <div
+                                    className="flex flex-row flex-wrap space-x-2 font-semibold font-poppins"
+                                    key={auteur.id}
+                                >
+                                    <h6>{auteur.nom} , </h6>
+                                    <div>{auteur.institutions.map(institution => {
                                         return (
-                                                <div
-                                                    className="flex flex-row flex-wrap space-x-2 font-semibold font-poppins"
-                                                    key={auteur.id}
-                                                    >
-                                                    <h6>{auteur.nom} , </h6>
-                                                    <div>{auteur.institutions.map(institution => {
-                                                        return (
-                                                            <h6 className="font-thin font-poppins"> {institution.nom}</h6>
+                                            <h6 className="font-thin font-poppins"> {institution.nom}</h6>
 
-                                                        )
-                                                    })} </div>
-                                                </div>
                                         )
-                                    })
-                                }
-                            </div>
-                        </div>
-                        <h1 className="mt-10 ml-12 text-xl font-bold font-poppins lg:ml-20 "> ABSTRACT </h1>
-                        <div className="font-poppins ml-12   lg:ml-20  w-[400px]  lg:w-[1180px] "> {article?.resume}</div>
-                        <h1 className="mt-8 ml-12 text-xl font-bold font-poppins lg:ml-20"> KEYWORDS </h1>
-                        <div
-                            className="flex  font-poppins text-sm  flex-row ml-12  w-[400px]  lg:w-[1180px] lg:ml-20 flex-wrap">
-                            {article?.mot_cles.map(motcle => {
-                                return (
-                                    <div className=""><h6 className=""> {motcle.text} , </h6></div>
-                                )
+                                    })} </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <h1 className="mt-10 ml-12 text-xl font-bold font-poppins lg:ml-20 "> ABSTRACT </h1>
+            <div className="font-poppins ml-12   lg:ml-20  w-[400px]  lg:w-[1180px] "> {article?.resume}</div>
+            <h1 className="mt-8 ml-12 text-xl font-bold font-poppins lg:ml-20"> KEYWORDS </h1>
+            <div
+                className="flex  font-poppins text-sm  flex-row ml-12  w-[400px]  lg:w-[1180px] lg:ml-20 flex-wrap">
+                {article?.mot_cles.map(motcle => {
+                    return (
+                        <div className=""><h6 className=""> {motcle.text} , </h6></div>
+                    )
 
-                            })}
-                            <div className="mt-14  w-[400px] lg:w-[1180px]">
-                                {article?.text_integral}
+                })}
+                <div className="mt-14  w-[400px] lg:w-[1180px]">
+                    {article?.text_integral}
+                </div>
+            </div>
+            <h1 className="mt-8 ml-12 text-xl font-bold font-poppins lg:ml-20 "> REFERENCES</h1>
+            <div className=" font-poppins text-sm   ml-12 lg:ml-20 w-[400px]  lg:w-[1180px] ">
+                {
+                    article?.references_bibliographique.map(ref => {
+                        return (
+                            <div className="flex flex-row" key={ref.id}>
+                                <h6 className="mt-4 "> [ {ref.id} ] {ref.nom}</h6>
                             </div>
-                        </div>
-                        <h1 className="mt-8 ml-12 text-xl font-bold font-poppins lg:ml-20 "> REFERENCES</h1>
-                        <div className=" font-poppins text-sm   ml-12 lg:ml-20 w-[400px]  lg:w-[1180px] ">
-                            {
-                                article?.references_bibliographique.map(ref => {
-                                    return (
-                                        <div className="flex flex-row" key={ref.id}>
-                                            <h6 className="mt-4 "> [ {ref.id} ] {ref.nom}</h6>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- src/pages/FavorisPage.tsx
+++ src/pages/FavorisPage.tsx
@@ -1,203 +1,143 @@
-import React,{useState} from 'react';
-import SearchBar from '../components/SearchBar';
-
-import logo from '../assets/Logo.svg'
-import Filters from '../components/Filters';
+import React, {useState} from 'react';
 import ArticleFavorisResult from '../components/ArticleFavorisResult';
-import { AiOutlineMail } from 'react-icons/ai';
+import {AiOutlineMail} from 'react-icons/ai';
+import NavBarUtilisateur from '../components/NavBarUtilisateur';
 
 interface Article {
-  title: string;
-  authors: string[];
-  institution: string;
-  abstract: string;
+    title: string;
+    authors: string[];
+    institution: string;
+    abstract: string;
 }
 
 const SearchPage: React.FC = () => {
+    const sampleArticles = [
+        {
+            title: 'Article 1',
+            authors: ['Author 1', 'Author 2'],
+            institution: 'Institution 1',
+            abstract: 'Abstract for Article 1...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
+        },
+        {
+            title: 'Article 2',
+            authors: ['Author 3', 'Author 4'],
+            institution: 'Institution 2',
+            abstract: 'Abstract for Article 2...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
+        },
+        {
+            title: 'Article 3',
+            authors: ['Author 3', 'Author 4'],
+            institution: 'Institution 2',
+            abstract: 'Abstract for Article 2...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
+        },
+        {
+            title: 'Article 3',
+            authors: ['Author 3', 'Author 4'],
+            institution: 'Institution 2',
+            abstract: 'Abstract for Article 2...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
+        },
+        {
+            title: 'Article 3',
+            authors: ['Author 3', 'Author 4'],
+            institution: 'Institution 2',
+            abstract: 'Abstract for Article 2...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
+        },
+        {
+            title: 'Article 3',
+            authors: ['Author 3', 'Author 4'],
+            institution: 'Institution 2',
+            abstract: 'Abstract for Article 2...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
+        },
+        // Add more articles as needed
+    ];
 
-  const [searchResultsCount, setSearchResultsCount] = useState<number>(0);
-  const [sortingOption, setSortingOption] = useState<string>('plusRecent');
-  const [searchResults, setSearchResults] = useState<Article[]>([]); // Assuming Article is a type/interface for your articles
+    const articlesToShow = sampleArticles.slice(0, 5);
 
-  const handleSearch = async (query: string) => {
-    try {
-      const response = await fetch(`/api/search?query=${query}`);
-      const data: Article[] = await response.json();
-      
-      setSearchResults(data);
-      setSearchResultsCount(data.length);
-    } catch (error) {
-      console.error('Error fetching search results:', error);
-    }
-  };
 
-  const handleClearAllFilters = () => {
-    // Implement logic to clear all filters
-    // For example, reset the state variables or make API calls to clear filters
-  };
-  
-  // Sample articles data (replace it with your actual data or API call)
-  const sampleArticles = [
-  {
-    title: 'Article 1',
-    authors: ['Author 1', 'Author 2'],
-    institution: 'Institution 1',
-    abstract: 'Abstract for Article 1...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
-  },
-  {
-    title: 'Article 2',
-    authors: ['Author 3', 'Author 4'],
-    institution: 'Institution 2',
-    abstract: 'Abstract for Article 2...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
-  },
-  {
-    title: 'Article 3',
-    authors: ['Author 3', 'Author 4'],
-    institution: 'Institution 2',
-    abstract: 'Abstract for Article 2...Abstract : Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ....',
-  },
-  // Add more articles as needed
-  ];
+    return (
+        <div className='Page'>
+            <div className='h-72 flex flex-col bg-[#EEF5FC]'>
+                <NavBarUtilisateur/>
+                <p className='my-auto text-2xl md:text-4xl font-semibold text-[#0053AD] text-center'>L'INFINI DU
+                    SAVOIR VOUS ATTEND A PORTEE DE CLIC.
+                </p>
+            </div>
 
-  const articlesToShow = sampleArticles.slice(0, 3); 
-  
-  const [showFilters, setShowFilters] = useState(false);
+            <div className='mb-6 p-4 border-y border-solid border-[#00000038]  bg-[#d9d9d91e] '>
+                <p className='ml-2 md:ml-12'>Accueil {'>'} Article Favoris </p>
+            </div>
 
-  const handleToggleFilters = () => {
-    setShowFilters(!showFilters);
-  };
+            <div className='flex flex-col Body md:flex-row'>
+                <div className=''>
+                    <div className=' lg:grid lg:grid-cols-3 lg:gap-4 px-10'>
+                        {articlesToShow.map((article, index) => (
+                            <ArticleFavorisResult
+                                key={index}
+                                title={article.title}
+                                authors={article.authors}
+                                institution={article.institution}
+                                abstract={article.abstract}
+                                onViewArticle={() => {
+                                    // Implement logic to view the article
+                                }}
+                                onAddToFavorites={() => {
+                                    // Implement logic to add to favorites
+                                }}
+                            />
+                        ))}
+                    </div>
+                </div>
 
- 
-  return (
-    <div className='Page'>
-     <div className='Header'>
-    <div className='bg-[#EEF5FC] p-6'>
-      <div className='flex mb-24'>
-        <img src={logo} alt='logo'/><span className=' text-xl font-bold'>Truth Finder</span>
-        <p className='mx-32 text-xl font-medium  '>Accueil</p>
-        <p className=' text-xl font-medium '>Article Favoris</p>
-        <p className=' ml-auto text-xl font-medium '> Username </p>
-      </div>
-      <p className='mb-4 md:mb-24 text-2xl md:text-4xl font-medium text-[#0053AD] text-center'>L'INFINI DU SAVOIR VOUS ATTEND A PORTEE DE CLIC.</p> 
-    </div>
-    
-    </div>
+            </div>
 
-    <div className='flex justify-center'>
-     <SearchBar onSearch={handleSearch} />
-    </div>
-     
-     <div className='my-6 p-4 border-y border-solid border-[#00000038]  bg-[#d9d9d91e] '>
-        <p className='ml-2 md:ml-12'>Accueil {'>'} Recherche </p>
-     </div>
+            <div className='p-20 PREVIOUS '></div>
 
-    <div className='Body  flex flex-col md:flex-row'>
+            <div className='Footer flex flex-col md:flex-row justify-between bg-[#EEF5FC]'>
 
-    <div>
-<button
-  className={`md:hidden border p-2 rounded ml-4 mb-4 ${showFilters ? 'bg-blue-500 text-white' : 'bg-gray-300'}`}
-  onClick={handleToggleFilters}
->
-  FILTRER RESULTAT
-</button>
-</div>
+                <div className='md:ml-20'>
+                    <p className='mt-8 mb-6 font-medium text-center md:mb-10 md:text-left'>QUI SOMMES NOUS</p>
+                    <div className='flex justify-center mb-6 md:mb-10'>
+                        <img className='font-medium' src={logo} alt='logo'/><span
+                        className='font-bold'>Truth Finder</span>
+                    </div>
+                    <p className='mb-4 font-medium text-center md:mb-20 md:text-left'>L'INFINITE DU SAVOIR <br/>VOUS
+                        ATTEND A <br/>PORTEE DU CLIC.</p>
+                </div>
 
-  {/* Responsive Filters Section */}
-  <div className={`Filters ${showFilters ? 'block' : 'hidden'} md:block w-full md:w-1/3`}>
-        <div className='flex  items-center'>
-          <p className='mb-4  ml-4 md:ml-16 text-xl text-[#00000080] font-medium' >FILTRES</p>
-          <p className='mb-4  ml-72 md:ml-52 text-xl text-[#ff020280] cursor-pointer font-medium' onClick={handleClearAllFilters}>
-            CLEAR ALL
-          </p>
-        </div>
-        <div className="mx-4 md:mx-16 mb-12 border-t border-solid border-[#00000080]"></div>
-        <Filters />
-      </div>
+                <div>
+                    <p className='my-8 font-medium text-center cursor-pointer'>SIGNALER ERREUR</p>
+                </div>
 
-    <div className="Results w-full md:w-2/3 p-4">
+                <div className='mb-4 text-center md:mb-0'>
+                    <p className='my-4 font-medium md:my-8'>SECTIONS</p>
+                    <p className='text-[#717171] font-medium mb-4'>ACCUEIL</p>
+                    <p className='text-[#717171] font-medium mb-4'>ARTICLES FAVORIS</p>
+                    <p className='text-[#717171] font-medium mb-4'>RECHERCHE</p>
+                </div>
 
-    <div className="flex justify-between items-center mb-6 md:mb-16 border-b p-2 md:p-6 border-[#00000080]">
-      <p className="text-sm md:text-lg">
-        {searchResultsCount} Résultats pour la recherche de "{/* Include the search query here */}"
-      </p>
-      <div className="flex items-center">
-        <p className="mr-2">Trier par :</p>
-        <select
-          value={sortingOption}
-          onChange={(e) => setSortingOption(e.target.value)}
-          className="border p-1 rounded-md bg-[#EEF5FC]"
-        >
-          <option value="plusRecent">Plus récent</option>
-          <option value="plusAncien">Plus ancien</option>
-          {/* Add more sorting options as needed */}
-        </select>
-      </div>
-    </div>
-        <div className=' lg:grid lg:grid-cols-2 lg:gap-4'>
-          {articlesToShow.map((article, index) => (
-            <ArticleFavorisResult
-              key={index}
-              title={article.title}
-              authors={article.authors}
-              institution={article.institution}
-              abstract={article.abstract}
-              onViewArticle={() => {
-                // Implement logic to view the article
-              }}
-              onAddToFavorites={() => {
-                // Implement logic to add to favorites
-              }}
-            />
-          ))}
-          </div>
+                <div className='mb-20 text-center md:mr-20 md:text-left'>
+                    <p className='my-4 font-medium md:my-8 '>S'ABONNER</p>
+                    <p className='text-[#717171] font-medium mb-6'>ENTRER VOTRE EMAIL<br/> POUR ETRE NOTIFIE SUR <br/>LES
+                        NOUVEUX ARTICLES <br/>DISPONIBLES</p>
+                    <div className="relative">
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            className="border rounded p-2 w-64 bg-[#C9CED6]"
+                        />
+                        <div
+                            className="absolute inset-y-0 right-0 flex items-center hidden pr-2 pointer-events-none md:flex">
+                            <AiOutlineMail className="text-2xl text-gray-500"/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
 
-    </div> 
-
-    <div className='PREVIOUS  p-20 '></div>
-
-    <div className='Footer flex flex-col md:flex-row justify-between bg-[#EEF5FC]'>
-
-<div className='md:ml-20'>
-  <p className='font-medium mt-8 mb-6 md:mb-10 text-center md:text-left'>QUI SOMMES NOUS</p>
-  <div className='flex justify-center mb-6 md:mb-10'>
-    <img className='font-medium' src={logo} alt='logo' /><span className='font-bold'>Truth Finder</span>
-  </div>
-  <p className='font-medium mb-4 md:mb-20 text-center md:text-left'>L'INFINITE DU SAVOIR <br />VOUS ATTEND A <br />PORTEE DU CLIC.</p>
-</div>
-
-<div>
-  <p className='font-medium my-8 cursor-pointer text-center'>SIGNALER ERREUR</p>
-</div>
-
-<div className='text-center mb-4 md:mb-0'>
-  <p className='font-medium my-4 md:my-8'>SECTIONS</p>
-  <p className='text-[#717171] font-medium mb-4'>ACCUEIL</p>
-  <p className='text-[#717171] font-medium mb-4'>ARTICLES FAVORIS</p>
-  <p className='text-[#717171] font-medium mb-4'>RECHERCHE</p>
-</div>
-
-<div className='md:mr-20 text-center md:text-left mb-20'>
-  <p className='font-medium my-4 md:my-8 '>S'ABONNER</p>
-  <p className='text-[#717171] font-medium mb-6'>ENTRER VOTRE EMAIL<br /> POUR ETRE NOTIFIE SUR <br />LES NOUVEUX ARTICLES <br />DISPONIBLES</p>
-  <div className="relative">
-  <input
-    type="email"
-    placeholder="Enter your email"
-    className="border rounded p-2 w-64 bg-[#C9CED6]"
-  />
-  <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none hidden md:flex">
-    <AiOutlineMail className="text-gray-500 text-2xl" />
-  </div>
-</div>
-</div>
-
-</div>
-
-
-     </div>  
-    
-  );
+    );
 };
 
 export default SearchPage;
